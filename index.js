015e1c33ff912840cc157631fba963b956c8fc62
aaaBUTCHER <aa61491@ubt-uni.net>
2023-06-08 21:01:17 +0100

    +

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path=require("path");
 const port= 3000;
 
+
 //Routerat
 const homepage=require("./routes/homepage");
 const bookCollection=require("./routes/bookCollection");
@@ -10,6 +11,9 @@ const crudiPerLibra=require("./routes/crudiPerLibra");
 const libratEBlere=require('./routes/libratEBlere');
 const about=require("./routes/about");
 const userProfile=require("./routes/userProfile");
+const signIn=require("./routes/sign-in");
+const register=require("./routes/register");
+
 
 //Expressi the connfigat e tij
 const express=require("express");
@@ -19,6 +23,8 @@ app.set("view engine", path.join(__dirname, "views"));
 app.set("view engine","pug");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+
 //pathat kryesor
 app.use("/", homepage);
 app.use("/book-collection", bookCollection);
@@ -28,11 +34,15 @@ app.use('/dashboard', dashboard);
 app.use("/crudiPerLibra", crudiPerLibra);
 app.use('/libratEBlere', libratEBlere);
 app.use("/user-profile", userProfile);
+app.use("/sign-in",signIn);
+app.use("/register", register);
+
 
 //midleware per err
 app.use((req, res,next)=>{
     res.status(404).render("err404");
 })
+
 
 //eventListner
 app.listen(port,()=>console.log("Po nijn ne porten "+ port));
