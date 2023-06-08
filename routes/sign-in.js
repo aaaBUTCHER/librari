89ef143bfb640d5da74991be957af94a1a8e7c18
aaaBUTCHER <aa61491@ubt-uni.net>
2023-06-08 15:36:55 +0100

    tu munu me ba najsen po spe rrok

--- routes/sign-in.js
+++ routes/sign-in.js
@@ -2,7 +2,7 @@ const express= require('express');
 const router= express.Router();
 
 router.get("/", (req,res)=>{
-    res.render("authentication/log-in");
+    res.render("authentication/sign-in");
 });
 
 module.exports=router;
